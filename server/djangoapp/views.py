e96f9231a06dca6a7681529c0df4352782019538
veronica-barreira <veronik.barreira@gmail.com>
2024-06-20 11:51:52 -0400

    Adding Django Proxy Services of Backend APIs

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -84,46 +84,78 @@ def registration(request):
 
 
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
+# `get_dealerships` view to render the index page with a list of dealerships
+def get_dealerships(request, state="ALL"):
+    if (state == "ALL"):
+        endpoint = "/fetchdealers"
+
+    else:
+        endpoint = "/fetchDealers/"+state
+    
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status": 200, "dealers": dealerships})
+
+
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+def get_dealer_reviews(request,dealer_id):
+    if (dealer_id):
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status": 200, "reviews": reviews})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
 
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
 
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+# `get_dealer_details` view to render the dealer details
+def get_dealer_details(request, dealer_id):
+    if (dealer_id):
+        endpoint = "/fetchDealer/"+str(dealer_id)
+        dealership = get_request(endpoint)
+        return JsonResponse({"status": 200, "dealer": dealership})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
+# `add_review` view to submit a review
+def add_review(request):
+    if (request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status": 200})
+        except:
+            return JsonResponse({"status": 401, "message": "Error in posting review"})
+    else:
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
+
 
 def get_cars(request):
     # Clear the existing data in Carmodel and CarMake
-    CarModel.objects.all().delete()
-    CarMake.objects.all().delete()
+    # CarModel.objects.all().delete()
+    # CarMake.objects.all().delete()
 
-    # Call the initiate function to repopulate the data
-    initiate()
+    # # Call the initiate function to repopulate the data
+    # initiate()
 
-    # Fetch the newly populated data
-    car_models = CarModel.objects.select_related('car_make')
-    cars = []
-    for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    
-    return JsonResponse({"CarModels": cars})
-    
-    # count = CarMake.objects.filter().count()
-    # print(count)
-    # if (count == 0):
-    #     initiate()
+    # # Fetch the newly populated data
     # car_models = CarModel.objects.select_related('car_make')
     # cars = []
     # for car_model in car_models:
     #     cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    
     # return JsonResponse({"CarModels": cars})
+    
+    count = CarMake.objects.filter().count()
+    print(count)
+    if (count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels": cars})
 
